7bc4d6fbcd522395d70b82185eccf4a197fcb1fc
nopnapatn <nopnapatn@gmail.com>
2024-01-25 16:05:25 +0700

    feat(*): initial project

---
+++ drain/drainApp.swift
@@ -0,0 +1,17 @@
+//
+//  drainApp.swift
+//  drain
+//
+//  Created by Nopnapat Norasri on 25/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct drainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
