d40d97d5a566517243e8ae7c6e7148c163bf37ab
nopnapatn <nopnapatn@gmail.com>
2024-01-28 20:38:53 +0700

    perf(*): add firebase to project drain

---
+++ drain/App/drainApp.swift
@@ -0,0 +1,28 @@
+//
+//  drainApp.swift
+//  drain
+//
+//  Created by Nopnapat Norasri on 25/1/2567 BE.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct drainApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
